7dd412f6609386ebf1f45f5c5f22072c773f7e82
Jordan Ling <anotherjordanishere@gmail.com>
2024-12-09 14:05:02 +0800

    it now reads the text file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+
+
+
 int main() {
-    cout << "hello world" << endl;
+    ifstream file("game.txt");
+
+    if (!file.is_open()) {
+        cout << "File was unable to be opened, ensure you are in the correct directory/ the file exists." << endl;
+        return 1;
+    } else {
+        cout << "File is open!" << endl;
+    }
+
+    int iterations, width, height, battleships, cruisers, destroyers, frigates, corvettes, amphbious;
+    int counter = 0;
+    string line;
+
+    while (getline(file, line)) {
+        cout << line << endl;
+
+        counter++;
+    }
 }
