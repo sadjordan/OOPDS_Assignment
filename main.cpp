e91759ea13ceb3b90810f493bc2fc8e27f80a7bc
Jordan Ling <anotherjordanishere@gmail.com>
2024-12-30 11:39:20 +0800

    Random placement is fixed

--- main.cpp
+++ main.cpp
@@ -42,7 +42,9 @@ public:
         }
 
         // team A
-        int* TeamA = new int[size_a];
+        int* TeamA = new int[size_a + 1];
+        char* TeamA_symbols = new char[size_a];
+        TeamA[0] = size_a;
 
         file.clear();
         file.seekg(0, ios::beg);
@@ -60,8 +62,13 @@ public:
             while (iss >> word && counter < 4 + size_a) {
                 istringstream wordStream(word);
                 if (wordStream >> value) {
-                    TeamA[counter - 4] = value;
+                    TeamA[counter - 3] = value;
                     cout << "value: " << value << endl;
+                }
+
+                if (word.size() == 1 && !isdigit(word[0])) {
+                    TeamA_symbols[counter - 4] = word[0];
+                    cout << word[0] << endl;
                 }
             }
 
@@ -74,12 +81,18 @@ public:
             counter++;
         }
 
-        for (int i = 0; i < size_a; i++) {
+        for (int i = 0; i < size_a + 1; i++) {
             cout << i << ": " << TeamA[i] << endl;
         }
 
+        for (int i = 0; i < TeamA[0]; i++) {
+            cout << i << ": " << TeamA_symbols[i] << endl;
+        }
+
         //team b
-        int* TeamB = new int[size_b];
+        int* TeamB = new int[size_b + 1];
+        char* TeamB_symbols = new char[size_b];
+        TeamB[0] = size_b;
 
         file.clear();
         file.seekg(0, ios::beg);
@@ -98,17 +111,25 @@ public:
             while (iss >> word) {
                 istringstream wordStream(word);
                 if (wordStream >> value) {
-                    TeamB[counter - 5 - size_a] = value;
+                    TeamB[counter - 4 - size_a] = value;
                     cout << "value: " << value << endl;
+                }
+
+                if (word.size() == 1 && !isdigit(word[0])) {
+                    TeamB_symbols[counter - 5 - size_a] = word[0];
+                    cout << word[0] << endl;
                 }
             }
             counter++;
         }
 
-        for (int i = 0; i < size_b; i++) {
+        for (int i = 0; i < TeamB[0] + 1; i++) {
             cout << i << ": " << TeamB[i] << endl;
         }
 
+        for (int i = 0; i < TeamB[0]; i++) {
+            cout << i << ": " << TeamB_symbols[i] << endl;
+        }
 
         int rows = game_settings[1], columns = game_settings[2];
         char** game_map = new char*[rows];
@@ -155,11 +176,11 @@ public:
             }
             cout << endl;
         }
-        random_placement(game_settings, game_map);
+        random_placement(game_settings, game_map, TeamA, TeamB, TeamA_symbols, TeamB_symbols);
     }
 
-    void random_placement(int* game_settings, char** game_map) {
-    char symbols[] = {'*', '$', '#', '@', '&', '~'};
+    void random_placement(int* game_settings, char** game_map, int* TeamA, int* TeamB, char* TeamA_symbols, char* TeamB_symbols) {
+    //char symbols[] = {'*', '$', '#', '@', '&', '~'};
     int x, y;
 
     random_device rd;
@@ -168,20 +189,48 @@ public:
     //debug stuff
     //int symbols_placed[] = {0, 0, 0, 0, 0, 0};
 
-    for (int i = 3; i < 9; i++) {
+    for (int i = 1; i < TeamA[0] + 1; i++) {
         uniform_int_distribution<> x_dis(0, game_settings[1] - 1);
         uniform_int_distribution<> y_dis(0, game_settings[2] - 1);
 
-        while (game_settings[i]-- > 0) {
+        while (TeamA[i]-- > 0) {
             
             do {
                 x = x_dis(gen);
                 y = y_dis(gen);
-            } while(game_map[x][y] != '0');
-            game_map[x][y] = symbols[i - 3];
+            } while(game_map[x][y] != '0'); // prevents placement on islands/ occupied coords
+            game_map[x][y] = TeamA_symbols[i - 1];
             
-            //symbols_placed[i-3]++;
-            cout << "Placed " << symbols[i - 3] << " at (" << x + 1 << ", " << y + 1 << ")" << endl;
+            //symbols_placed[i]++;
+            cout << "Placed " << TeamA_symbols[i - 1] << " at (" << x + 1 << ", " << y + 1 << ")" << endl;
+
+            for (int i = 0; i < game_settings[1]; ++i) {
+                for (int j = 0; j < game_settings[2]; ++j) {
+                    cout << game_map[i][j] << " ";
+                }
+                cout << endl;
+            }
+
+            //for (auto i : symbols_placed) {
+            //   cout << i << endl;
+            //}
+        }
+    }
+
+    for (int i = 1; i < TeamB[0] + 1; i++) {
+        uniform_int_distribution<> x_dis(0, game_settings[1] - 1);
+        uniform_int_distribution<> y_dis(0, game_settings[2] - 1);
+
+        while (TeamB[i]-- > 0) {
+            
+            do {
+                x = x_dis(gen);
+                y = y_dis(gen);
+            } while(game_map[x][y] != '0'); // prevents placement on islands/ occupied coords
+            game_map[x][y] = TeamB_symbols[i - 1];
+            
+            //symbols_placed[i]++;
+            cout << "Placed " << TeamB_symbols[i - 1] << " at (" << x + 1 << ", " << y + 1 << ")" << endl;
 
             for (int i = 0; i < game_settings[1]; ++i) {
                 for (int j = 0; j < game_settings[2]; ++j) {
